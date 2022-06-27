b645a4944441a1ed880a08625b820941dfdb3326
Eugene kodzila <simba.eugene@gmail.com>
2022-06-28 00:00:13 +0300

    1)Создал новый проект 2)В класс импортировал CoreLocation 3)Прописал делегат CLLocationManagerDelegate 4)Реализовал методы делегата 5)Написал функцию для получения данных о местоположении 6)В сториборд добавил UILabel, расположил по середине 7)Полученные данные о местоположении вынес в UILabel 8)В info.рlist  прописал разрешения на отслеживание местоположения

--- TestWorkSiberian2/ViewController.swift
+++ TestWorkSiberian2/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var label: UILabel!
+    var manager: CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "ready!"
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let first = locations.first else {return}
+        label.text = "\(first.coordinate.longitude) | \(first.coordinate.latitude)"
+    }
 }
+
 
