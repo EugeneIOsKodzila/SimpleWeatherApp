73b48e08c729fff1da61c3d71bdf746440d7ba1f
Eugene kodzila <simba.eugene@gmail.com>
2022-06-27 17:26:33 +0300

    Initial Commit

---
+++ TestWorkSiberian2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWorkSiberian2
+//
+//  Created by Наташа on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
